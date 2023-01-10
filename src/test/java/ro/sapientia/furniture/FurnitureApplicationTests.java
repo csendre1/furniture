fbf7ac84988acfdc712ad3767d6f1e2d222a0e2a
kollozsolt <zsoltkollo67@gmail.com>
2023-01-10 07:19:09 +0200

    bdtTests - not working

--- src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
+++ src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
@@ -3,10 +3,8 @@ package ro.sapientia.furniture;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FurnitureApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
